b2d844765cd3e9c5517efe3d26a76496e67213d7
bakdaurenNarbayev <nbakdauren@gmail.com>
2021-03-09 03:05:44 +0600

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  else return array.reduce((accumulator, currentValue) => Math.min(accumulator, currentValue), array[0]);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  else return array.reduce((accumulator, currentValue) => Math.max(accumulator, currentValue), array[0]);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  else return array.reduce((accumulator, currentValue) => accumulator + currentValue) / array.length;
 }
